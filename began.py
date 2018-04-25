728c14a5eac1d6e7011b1f0b6d4cb82261f5d444
amyncarol <caiyao214@gmail.com>
2018-04-25 12:42:48 -0700

    sd

--- began.py
+++ began.py
@@ -107,14 +107,14 @@ if __name__ == '__main__':
 	#os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 	# save generated images
-	sample_dir = 'Samples/began_imaterials_12'
+	sample_dir = 'Samples/began_celebA_2000'
 	if not os.path.exists(sample_dir):
 		os.makedirs(sample_dir)
 
 	# param
 	generator = G_conv()
 	discriminator = D_autoencoder()
-	data = imaterials(12)
+	data = celebA()
 
 	# run
 	began = BEGAN(generator, discriminator, data)
