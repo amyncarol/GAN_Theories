4eea55272ffad9a017fd0fc5273689cfed4dddc9
amyncarol <caiyao214@gmail.com>
2018-04-30 15:41:29 -0700

    added began2

---
+++ began2.py
@@ -0,0 +1,148 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os,sys
+
+sys.path.append('utils')
+from nets import *
+from data_utils import parser, data2fig
+
+Z_DIM = 100
+BATCH_SIZE = 16
+GAMMA = 1
+LAMBDA_K = 0.002
+
+def sample_z(m, n):
+    return np.random.uniform(-1., 1., size=[m, n])
+
+class BEGAN():
+    def __init__(self, generator, discriminator, tfrecord_names, model_path):
+        self.generator = generator
+        self.discriminator = discriminator
+        self.model_path = model_path
+
+        # data
+        self.z_dim = Z_DIM
+
+        # training dataset
+        dataset = tf.data.TFRecordDataset(tfrecord_names)
+        dataset = dataset.map(parser)
+        dataset = dataset.batch(BATCH_SIZE)
+        dataset = dataset.repeat()
+        iterator = dataset.make_one_shot_iterator()
+        images, labels = iterator.get_next()
+
+        self.X = images
+        self.z = tf.placeholder(tf.float32, shape=[None, self.z_dim])
+
+        # began parameters
+        self.k_t =  tf.placeholder(tf.float32, shape=[]) # weighting parameter which constantly updates during training
+        gamma = GAMMA  # diversity ratio, used to control model equibilibrium.
+        lambda_k = LAMBDA_K # learning rate for k. Berthelot et al. use 0.001
+
+        # nets
+        self.G_sample = self.generator(self.z)
+
+        self.D_real = self.discriminator(self.X)
+        self.D_fake = self.discriminator(self.G_sample, reuse = True)
+        
+        # loss
+        L_real = tf.reduce_mean(tf.abs(self.X - self.D_real))
+        L_fake = tf.reduce_mean(tf.abs(self.G_sample - self.D_fake))
+
+        self.D_loss = L_real - self.k_t * L_fake
+        self.G_loss = L_fake
+        
+        self.k_tn = self.k_t + lambda_k * (gamma*L_real - L_fake)
+        self.M_global = L_real + tf.abs(gamma*L_real - L_fake)      
+    
+        # solver
+        self.learning_rate = tf.placeholder(tf.float32, shape=[], name='learning_rate')
+        self.D_solver = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.D_loss, var_list=self.discriminator.vars)
+        self.G_solver = tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.G_loss, var_list=self.generator.vars)
+        
+        self.saver = tf.train.Saver()
+        gpu_options = tf.GPUOptions(allow_growth=True)
+        self.sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+        self.model_name = os.path.join(self.model_path, 'began.ckpt')
+
+        ##tf summary
+        tf.summary.scalar('M_global', self.M_global)
+        tf.summary.scalar('k_t', self.k_t)
+        tf.summary.scalar('L_real', L_real)
+        tf.summary.scalar('G_loss', self.G_loss)
+        tf.summary.scalar('D_loss', self.D_loss)
+        #tf.summary.scalar('learning_rate', self.learning_rate)
+        self.summary = tf.summary.merge_all()
+        self.summary_writer = tf.summary.FileWriter(self.model_path, self.sess.graph)
+
+    def train(self, sample_dir, training_epoches = 500000, batch_size = BATCH_SIZE):
+        fig_count = 0
+        self.sess.run(tf.global_variables_initializer())
+        #self.saver.restore(self.sess, self.model_name)     
+
+        k_tn = 0
+        learning_rate_initial = 1e-4
+        for epoch in range(training_epoches):
+            learning_rate =  learning_rate_initial * pow(0.5, epoch // 50000)
+            # update D and G
+            _, _, k_tn = self.sess.run(
+                [self.D_solver, self.G_solver, self.k_tn],
+                feed_dict={self.z: sample_z(batch_size, self.z_dim), self.k_t: min(max(k_tn, 0.), 1.), self.learning_rate: learning_rate}
+                )
+            # save img, model. print loss
+            if epoch % 100 == 0 or epoch < 100:
+                D_loss_curr, G_loss_curr, M_global_curr, summary_str = self.sess.run(
+                        [self.D_loss, self.G_loss, self.M_global, self.summary],
+                        feed_dict={self.z: sample_z(batch_size, self.z_dim), self.k_t: min(max(k_tn, 0.), 1.)})
+                print('Iter: {}; D_loss: {:.4}; G_loss: {:.4}; M_global: {:.4}; k_t: {:.6}; learning_rate:{:.8}'.format(epoch, D_loss_curr, G_loss_curr, M_global_curr, min(max(k_tn, 0.), 1.), learning_rate))
+                self.summary_writer.add_summary(summary_str, epoch)
+                self.summary_writer.flush()
+
+                if epoch % 1000 == 0:
+                    X_s, real, samples = self.sess.run([self.X, self.D_real, self.G_sample], feed_dict={self.z: sample_z(BATCH_SIZE, self.z_dim)})
+
+                    fig = data2fig(X_s)
+                    plt.savefig('{}/{}.png'.format(sample_dir, str(fig_count).zfill(3)), bbox_inches='tight')
+                    plt.close(fig)
+
+                    fig = data2fig(real)
+                    plt.savefig('{}/{}_d.png'.format(sample_dir, str(fig_count).zfill(3)), bbox_inches='tight')
+                    plt.close(fig)
+
+                    fig = data2fig(samples)
+                    plt.savefig('{}/{}_r.png'.format(sample_dir, str(fig_count).zfill(3)), bbox_inches='tight')
+                    plt.close(fig)
+
+                    fig_count += 1
+
+                if epoch % 5000 == 0:
+                    self.saver.save(self.sess, self.model_name)
+
+if __name__ == '__main__':
+
+    # constraint GPU
+    #os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+    # input and output folders
+    sample_dir = 'Samples/began_imaterials_20_gamma1_lambda0002'
+    model_dir = 'Models/began_imaterials_20_gamma1_lambda0002'
+    tfrecord_names = ['Datas/imaterials_tfrecord/20.tfrecord']
+    
+    if not os.path.exists(sample_dir):
+        os.makedirs(sample_dir)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+
+    # param
+    generator = G_conv()
+    discriminator = D_autoencoder()
+
+    # run
+    began = BEGAN(generator, discriminator, tfrecord_names, model_dir)
+    began.train(sample_dir)
+
--- data_utils.py
+++ data_utils.py
@@ -5,71 +5,155 @@ import sys
 from PIL import Image
 import scipy.misc
 from glob import glob
+import tensorflow as tf 
+import time
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 
 FOLDERS = ['/Users/yao/large_data_file_no_sync/imaterials/train_chunk0', 
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk1',
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk2',
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk3',
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk4',
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk5',
-			'/Users/yao/large_data_file_no_sync/imaterials/train_chunk6',
-			'/Users/yao/large_data_file_no_sync/imaterials/valid']
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk1',
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk2',
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk3',
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk4',
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk5',
+            '/Users/yao/large_data_file_no_sync/imaterials/train_chunk6',
+            '/Users/yao/large_data_file_no_sync/imaterials/valid']
 
 PATH = '/Users/yao/large_data_file_no_sync/imaterials'
 
+IMAGE_SIZE = 64
+
 def get_folder_size(path):
-	return sum([len(files) for r, d, files in os.walk(path)]) - 1
+    return sum([len(files) for r, d, files in os.walk(path)]) - 1
 
 def get_class_size(i):
-	counter = 0
-	for folder in FOLDERS:
-		subfolder = os.path.join(folder, str(i))
-		counter += len(os.listdir(subfolder))
-	return counter
+    counter = 0
+    for folder in FOLDERS:
+        subfolder = os.path.join(folder, str(i))
+        counter += len(os.listdir(subfolder))
+    return counter
 
 def largest_n_class(n):
-	counter = np.zeros(128)
-	for i in range(1, 129):
-		counter[i-1]=get_class_size(i)
-	index = np.argsort(counter)
-	return index[-n:][::-1]+1
+    counter = np.zeros(128)
+    for i in range(1, 129):
+        counter[i-1]=get_class_size(i)
+    index = np.argsort(counter)
+    return index[-n:][::-1]+1
 
 def generate_class_folder(i):
-	os.mkdir(os.path.join(PATH, str(i)))
-	for folder in FOLDERS:
-		files = os.listdir(os.path.join(folder, str(i)))
-		for file in files:
-			file_path = os.path.join(folder, str(i)+'/'+file)
-			if os.path.isfile(file_path):
-				shutil.copy(file_path, os.path.join(PATH, str(i)))
+    os.mkdir(os.path.join(PATH, str(i)))
+    for folder in FOLDERS:
+        files = os.listdir(os.path.join(folder, str(i)))
+        for file in files:
+            file_path = os.path.join(folder, str(i)+'/'+file)
+            if os.path.isfile(file_path):
+                shutil.copy(file_path, os.path.join(PATH, str(i)))
 
 def resize_images_in_folder(folder, folder_resized, resize_h=64):
-	imgs_name = [i for i in os.listdir(folder) if i.endswith('jpeg')]
+    imgs_name = [i for i in os.listdir(folder) if i.endswith('jpeg')]
 
-	if not os.path.exists(folder_resized):
-		os.makedirs(folder_resized)
+    if not os.path.exists(folder_resized):
+        os.makedirs(folder_resized)
 
-	counter = 0
-	for img_name in imgs_name:
-		img = scipy.misc.imread(os.path.join(folder, img_name)).astype(np.float)
-		resize_w = resize_h
-		resized_image = scipy.misc.imresize(img,[resize_h, resize_w])
-		scipy.misc.imsave(os.path.join(folder_resized, img_name), resized_image)
+    counter = 0
+    for img_name in imgs_name:
+        img = scipy.misc.imread(os.path.join(folder, img_name)).astype(np.float)
+        resize_w = resize_h
+        resized_image = scipy.misc.imresize(img,[resize_h, resize_w])
+        scipy.misc.imsave(os.path.join(folder_resized, img_name), resized_image)
 
-		counter += 1
-		if counter % 100 == 0:
-			print(counter)
+        counter += 1
+        if counter % 100 == 0:
+            print(counter)
 
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def write_tfrecord(class_i, tfrecord_folder):
+    start_time = time.time()
+    counter = 0
+    writer = tf.python_io.TFRecordWriter(os.path.join(tfrecord_folder, str(class_i)+'.tfrecord'))
+    for folder in FOLDERS:
+        images = glob(os.path.join(folder, str(class_i)+'/*.jpeg'))
+        for image in images:
+            img = Image.open(image)
+            img = np.array(img.resize((IMAGE_SIZE, IMAGE_SIZE)))
+            label = class_i
+            feature = {'label': _int64_feature(label),
+                'image': _bytes_feature(img.tostring())}
+            example = tf.train.Example(features=tf.train.Features(feature=feature))
+            writer.write(example.SerializeToString())
+            counter += 1
+            if counter % 100 == 0:
+                print('processed {} image, which takes {} sec'.format(counter, time.time()-start_time))
+    writer.close()
+
+def parser(record):
+    """
+    Use `tf.parse_single_example()` to extract data from a `tf.Example`
+    protocol buffer, and perform any additional per-record preprocessing.
+    """
+    keys_to_features = {
+        'image': tf.FixedLenFeature([], tf.string, default_value=""),
+        'label': tf.FixedLenFeature([], tf.int64,
+                                default_value=tf.zeros([], dtype=tf.int64)),
+    }
+    parsed = tf.parse_single_example(record, keys_to_features)
+
+    # Perform additional preprocessing on the parsed data.
+    image = tf.decode_raw(parsed["image"], tf.uint8)
+    image = tf.cast(tf.reshape(image, [IMAGE_SIZE, IMAGE_SIZE, 3]), tf.float32)/255.0
+    label = tf.cast(parsed["label"], tf.int32)
+
+    return image, label
+
+def data2fig(samples):
+    fig = plt.figure(figsize=(4, 4))
+    gs = gridspec.GridSpec(4, 4)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample)
+    return fig
 
 if __name__=='__main__':
-	# class_i = largest_n_class(5)
-	# for i in class_i:
-	# 	generate_class_folder(i)
-	# 	print(get_class_size(i))
-	for i in [12, 42, 92, 125]:
-		resize_images_in_folder('/Users/yao/Google Drive/projects_ml/GAN_Theories/Datas/imaterials_original/'+str(i),\
-			'/Users/yao/Google Drive/projects_ml/GAN_Theories/Datas/imaterials/'+str(i))
+    # class_i = largest_n_class(5)
+    # for i in class_i:
+    #   generate_class_folder(i)
+    #   print(get_class_size(i))
+    #for i in [12, 42, 92, 125]:
+    #   resize_images_in_folder('/Users/yao/Google Drive/projects_ml/GAN_Theories/Datas/imaterials_original/'+str(i),\
+    #       '/Users/yao/Google Drive/projects_ml/GAN_Theories/Datas/imaterials/'+str(i))
+
+    #write_tfrecord(20, './Datas/imaterials_tfrecord')
+
+    filenames = ["./Datas/imaterials_tfrecord/20.tfrecord"]
+    dataset = tf.data.TFRecordDataset(filenames)
+    dataset = dataset.map(parser)
+    dataset = dataset.batch(16)
+    dataset = dataset.repeat()
+    iterator = dataset.make_one_shot_iterator()
+    images, labels = iterator.get_next()
+
+    with tf.Session() as sess:
+        img, lab = sess.run([images, labels])
+        print(img)
+        fig = data2fig(img)
+        plt.savefig('test.png', bbox_inches='tight')
+        plt.close(fig)
 
 
 
-	
+
+
+    
